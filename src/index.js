ad05dc572323f3d39a625b19fa100778f5fa64f2
Carlos Diaz <charliedn@gmail.com>
2020-09-17 18:13:52 +0100

    React app that displays a message depending the month of the year

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
+// import faker from 'faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+  state = { lat: null, long: null, errMsg: '' }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude, long: position.coords.longitude }),
+      err => this.setState({ errorMsg: err.message })
+    );
+  }
+
+  // componentDidUpdate() {
+  //   console.log('My compo was updated');
+  // }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>
+    }
+
+    if (!this.state.errorMsg && this.state.lat) {
+      return (
+        <SeasonDisplay
+          lat={this.state.lat}
+          long={this.state.long}
+          err="" />
+      )
+    }
+
+    return (<div>Loading...</div>);
+
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
